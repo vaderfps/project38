d63a625b086e8bdafccb7a856d24947ef2df4b1e
vaderfps <100806560+vaderfps@users.noreply.github.com>
2022-09-14 18:31:46 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+ 
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+   // clear();
+    quiz.play();
+  }
+}
+//NÃO CONSEGUI TIRAR DO 404 ESSE CODIGO, \_('-')_/
+//                                           |
+//                                           |
+//                                          /|\
+//                                            
